23b5c13563f9a96a8b90af969afa24f0e568b555
Peter Compernolle <me@petercompernolle.com>
2014-03-16 18:17:58 -0500

    adds more comments

--- clients/dundermifflin/InventionDecorator.php
+++ clients/dundermifflin/InventionDecorator.php
@@ -1,12 +1,26 @@
 <?php
 
+/**
+ * Decorate the Invention class for the dundermifflin client.
+ * For this example, the dunder mifflin client wants to track paper used by invention.
+ */
 class InventionDecorator_Client extends InventionDecorator implements iInvention {
     protected $decorated;
 
+    /**
+     * Get additional properties which will be applied to the Invention class
+     *
+     * @return array
+     */
     protected function additionalProperties() {
         return array('paper_used');
     }
 
+    /**
+     * Client specific method. Get the number of sheets of paper used on this invention
+     *
+     * @return string
+     */
     public function paperUsed() {
         return "This invention used " . $this->decorated->paper_used . " sheets of paper";
     }
--- plugins/project/InventionDecorator.php
+++ plugins/project/InventionDecorator.php
@@ -1,12 +1,27 @@
 <?php
 
+/**
+ * Decorate the Invention class for the project plugin.
+ * The project plugin adds a relationship between a project and an invention.
+ */
 class InventionDecorator_Project extends InventionDecorator implements iInvention {
     protected $decorated;
 
+    /**
+     * Get additional properties which will be applied to the Invention class
+     *
+     * @return array
+     */
     protected function additionalProperties() {
         return array('project');
     }
 
+    /**
+     * Attach a project to this invention
+     *
+     * @param int $project_id, the id of the project
+     * @return void
+     */
     public function attachProject($project_id) {
         $this->decorated->project = $project_id;
     }
