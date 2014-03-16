0592c1d638fe47227ed0991db53e9f84da188fd0
Peter Compernolle <me@petercompernolle.com>
2014-03-16 18:15:18 -0500

    adds overview comment for Invention.php model file

--- base_models/Invention.php
+++ base_models/Invention.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Example Object, as well as what is needed to decorate it
+ *
+ * includes:
+ *  * interface, which this object and all decorators MUST implement
+ *  * Invention class definition, where you can add methods or default properties
+ *  * abstract decorator class, which all decorators MUST extend
+ *
+ * Note that in a production app, these classes should probably belong in different files/places.
+ */
 
 interface iInvention {
     public function pre_save();
